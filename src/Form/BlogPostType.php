e3ef5987b83ae6a714c51b1622cb8ed6029530d8
Juks <juks@alkohol.ee>
2020-11-05 23:13:45 +0200

    login form and blogpost generated

---
+++ src/Form/BlogPostType.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\BlogPost;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BlogPostType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('body')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => BlogPost::class,
+        ]);
+    }
+}
